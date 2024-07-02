05fbc71a7949267c120bdbff206611c259006f33
kashish kapoor <kashish3102001@gmail.com>
2024-07-02 11:10:29 +0530

    commit

--- server.js
+++ server.js
@@ -1,34 +1,41 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const ShortUrl = require('./models/shortUrl')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const ShortUrl = require('./models/shortUrl');
+const app = express();
 
-mongoose.connect(' mongodb://127.0.0.1:27017/urlShortener', {
-  useNewUrlParser: true, useUnifiedTopology: true
-})
+// Load environment variables from .env file in development
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({ extended: false }))
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+});
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', async (req, res) => {
-  const shortUrls = await ShortUrl.find()
-  res.render('index', { shortUrls: shortUrls })
-})
+  const shortUrls = await ShortUrl.find();
+  res.render('index', { shortUrls: shortUrls });
+});
 
 app.post('/shortUrls', async (req, res) => {
-  await ShortUrl.create({ full: req.body.fullUrl })
-
-  res.redirect('/')
-})
+  await ShortUrl.create({ full: req.body.fullUrl });
+  res.redirect('/');
+});
 
 app.get('/:shortUrl', async (req, res) => {
-  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
-  if (shortUrl == null) return res.sendStatus(404)
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+  if (shortUrl == null) return res.sendStatus(404);
 
-  shortUrl.clicks++
-  shortUrl.save()
+  shortUrl.clicks++;
+  shortUrl.save();
 
-  res.redirect(shortUrl.full)
-})
+  res.redirect(shortUrl.full);
+});
 
-app.listen(process.env.PORT || 8000);
+app.listen(process.env.PORT || 8000, () => {
+  console.log(`Server running on port ${process.env.PORT || 8000}`);
+});
